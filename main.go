a96a66ca505d5c36fd7b3e13593f3912f5d6e257
Warmfilly <warm.filly@protonmail.com>
2022-11-21 19:55:50 -0700

    Basic table which displays files

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"io/fs"
+	"io/ioutil"
+	"log"
+
 	"github.com/rivo/tview"
 )
 
 func main() {
-	table := getTable()
+	table := getTable("/home/warm/")
 
 	if err := tview.NewApplication().SetRoot(table, true).Run(); err != nil {
 		panic(err)
 	}
 }
 
-func getTable() *tview.Table {
+func getTable(path string) *tview.Table {
 	table := tview.NewTable().
-		SetBorders(true).
-		SetCell(0, 0, tview.NewTableCell("Cell One")).
-		SetCell(0, 1, tview.NewTableCell("Cell Two")).
-		SetCell(1, 0, tview.NewTableCell("Cell Three")).
-		SetCell(1, 1, tview.NewTableCell("Cell Four"))
+		SetBorders(true)
+
+	table.SetFixed(1, 0)
+
+	table.SetCell(0, 0, tview.NewTableCell("Name"))
+	table.SetCell(0, 1, tview.NewTableCell("Size"))
+
+	for index, file := range iterateFiles(path) {
+		table.SetCell(index+1, 0, tview.NewTableCell(file.Name()))
+		table.SetCell(index+1, 1, tview.NewTableCell("10kb"))
+	}
 
 	return table
+}
+
+func iterateFiles(path string) []fs.FileInfo {
+	files, err := ioutil.ReadDir(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return files
 }
