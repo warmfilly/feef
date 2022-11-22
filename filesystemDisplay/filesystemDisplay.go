f0a4baa46289f1b60859bb9606e8b877bc607924
Warmfilly <warm.filly@protonmail.com>
2022-11-21 20:52:00 -0700

    Color files red, color dirs blue

--- filesystemDisplay/filesystemDisplay.go
+++ filesystemDisplay/filesystemDisplay.go
@@ -10,6 +10,37 @@ import (
 )
 
 func GetTable(path string) *tview.Table {
+	table := initTable()
+
+	for index, file := range iterateFiles(path) {
+		var nameCell *tview.TableCell
+
+		if file.IsDir() {
+			nameCell = dirCell(file.Name())
+		} else {
+			nameCell = fileCell(file.Name())
+		}
+		table.SetCell(index+1, 0, nameCell)
+
+		sizeStr := humanize.Bytes(uint64(file.Size()))
+		table.SetCell(index+1, 1, tview.NewTableCell(sizeStr))
+
+	}
+
+	return table
+}
+
+func fileCell(name string) *tview.TableCell {
+	cell := tview.NewTableCell("[red]" + name)
+	return cell
+}
+
+func dirCell(name string) *tview.TableCell {
+	cell := tview.NewTableCell("[blue]" + name)
+	return cell
+}
+
+func initTable() *tview.Table {
 	table := tview.NewTable().
 		SetBorders(true)
 
@@ -17,14 +48,6 @@ func GetTable(path string) *tview.Table {
 
 	table.SetCell(0, 0, tview.NewTableCell("Name"))
 	table.SetCell(0, 1, tview.NewTableCell("Size"))
-
-	for index, file := range iterateFiles(path) {
-		table.SetCell(index+1, 0, tview.NewTableCell(file.Name()))
-
-		sizeInt := uint64(file.Size())
-		sizeStr := humanize.Bytes(sizeInt)
-		table.SetCell(index+1, 1, tview.NewTableCell(sizeStr))
-	}
 
 	return table
 }
